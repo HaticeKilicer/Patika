814b1b2f53703afb5d1390b81b357882a4eff260
HaticeKilicer <79077901+HaticeKilicer@users.noreply.github.com>
2022-05-15 14:58:01 +0300

    Add files via upload

--- cinzodyagi/src/Main.java
+++ cinzodyagi/src/Main.java
@@ -7,48 +7,12 @@ public class Main {
         Scanner input = new Scanner(System.in);
 
         System.out.print("Yil Giriniz: ");
-        yil= input.nextInt();
+        yil = input.nextInt();
 
+        if(yil%400==0)
+            System.out.println(yil+" artik bir yildir!'");
+        else if(yil%4==0)
+            System.out.println(yil+" artik bir yildir!");
 
-        switch (burc){
-            case 0:
-                System.out.println("Cin Zodyagi Burcunuz: Maymun");
-                break;
-            case 1:
-                System.out.println("Cin Zodyagi Burcunuz: Horoz");
-                break;
-            case 2:
-                System.out.println("Cin Zodyagi Burcunuz: Kopek");
-                break;
-            case 3:
-                System.out.println("Cin Zodyagi Burcunuz: Domuz");
-                break;
-            case 4:
-                System.out.println("Cin Zodyagi Burcunuz: Fare");
-                break;
-            case 5:
-                System.out.println("Cin Zodyagi Burcunuz: Okuz");
-                break;
-            case 6:
-                System.out.println("Cin Zodyagi Burcunuz: Kaplan");
-                break;
-            case 7:
-                System.out.println("Cin Zodyagi Burcunuz: Tavsan");
-                break;
-            case 8:
-                System.out.println("Cin Zodyagi Burcunuz: Ejderha");
-                break;
-            case 9:
-                System.out.println("Cin Zodyagi Burcunuz: Yilan");
-                break;
-            case 10:
-                System.out.println("Cin Zodyagi Burcunuz: At");
-                break;
-            case 11:
-                System.out.println("Cin Zodyagi Burcunuz: Koyun");
-                break;
-            default:
-                System.out.println("Lutfen 0'dan buyuk bir yil giriniz!");
-        }
     }
 }
